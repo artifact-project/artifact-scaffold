b5e9f4a21b9875875058fc645f5847833cfc3c8f
RubaXa <k.lebedev@corp.mail.ru>
2017-09-02 09:45:38 +0300

    + decache

--- __server__/routing/configure.ts
+++ __server__/routing/configure.ts
@@ -1,26 +1,28 @@
 import {Context} from 'koa';
 import {runtimeBlockActivate} from '@exility/string';
 import {getUsedCSS} from '@exility/css';
+
 import now = require('performance-now');
-
-import Page from '../../src/blocks/Page/Page';
-import {getGlobalData} from '../../src/data/global';
-
-runtimeBlockActivate(Page, {
-	metaComments: true,
-});
+import decache = require('decache');
 
 export default (app) => {
 	app.use((ctx: Context) => {
 		const start = now();
 
+		const Page = require('../../src/blocks/Page/Page').default;
+		const {getGlobalData} = require('../../src/data/global');
+
+		runtimeBlockActivate(Page, {
+			metaComments: true,
+		});
+
 		const page = new Page(getGlobalData(ctx.req.url));
-		const content = page['__view__'];
 		const renderEnd = now();
+
 		const usedCSS = getUsedCSS();
 		const cssEnd = now();
 
-		ctx.body = content
+		ctx.body = page['__view__']
 			.replace('%__USED_CSS__%', usedCSS.names.join(','))
 			.replace('%__USED_CSS_TEXT__%', usedCSS.cssText)
 		;
@@ -28,5 +30,8 @@ export default (app) => {
 		ctx.set('x-time-render', (renderEnd - start) + '');
 		ctx.set('x-time-css', (cssEnd - renderEnd) + '');
 		ctx.set('x-time-all', (now() - start) + '');
+
+		decache('../../src/blocks/Page/Page');
+		decache('../../src/data/global');
 	});
 };
